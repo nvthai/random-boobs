b1796cda90dceb73524bc92df8e030b7fc8d8619
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-04 08:05:07 +0000

    Deploy nvthai/random-boobs to github.com/nvthai/random-boobs.git:gh-pages

---
+++ precache-manifest.3cb9f7419bf034a3d9b86adb68f02081.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "01b19fb19dfd73b8d7ce",
+    "url": "/random-boobs/static/css/main.2077ada5.chunk.css"
+  },
+  {
+    "revision": "01b19fb19dfd73b8d7ce",
+    "url": "/random-boobs/static/js/main.01b19fb1.chunk.js"
+  },
+  {
+    "revision": "0b6faec71e8755f7f40d",
+    "url": "/random-boobs/static/js/runtime~main.0b6faec7.js"
+  },
+  {
+    "revision": "713f9b0cbc070fe27625",
+    "url": "/random-boobs/static/js/2.713f9b0c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-boobs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d80ea8c6f1e0ec569b05165654dd8aa5",
+    "url": "/random-boobs/static/media/loading-balls.d80ea8c6.svg"
+  },
+  {
+    "revision": "f575b002ecc508b9cee0615a47501e1c",
+    "url": "/random-boobs/index.html"
+  }
+];
